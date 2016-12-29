23f9f93257443f7d7d40b209d941223600aa5895
Stefan Nožinić <stefan@lugons.org>
2016-12-29 15:38:50 +0100

    Exit after getting help

--- poison.py
+++ poison.py
@@ -4,6 +4,7 @@ import sys
 
 if sys.argv[1] == "--help":
     print("Usage: poison.py A_hw A_ip B_hw B_ip M_hw to poison between A and B with your device M")
+    exit(0)
 A_hw = sys.argv[1]
 A_ip = sys.argv[2]
 B_hw = sys.argv[3]
